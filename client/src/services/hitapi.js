6182bac4d59b46c59f435548cfb1f705e842c368
Sumankark <138770096+Sumankark@users.noreply.github.com>
2024-10-06 16:17:52 +0545

    Update hitapi.js

--- client/src/services/hitapi.js
+++ client/src/services/hitapi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export let hitApi = axios.create({
-  baseURL: "http://localhost:8080",
-  //   baseURL: "https://book-store-server-tks6.onrender.com",
+ // baseURL: "http://localhost:8080",
+  baseURL: "https://book-store-0f43.onrender.com",
 });
